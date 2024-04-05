10f52fbc5fdf478f52e5aeefa80bc55007a9afa2
LathanChen <czm316@live.com>
2024-04-05 22:51:16 +0900

    测试仓库私有化后的提交效果

--- src/pages/AdminPageAddItempage/AdminPageAddItempage.js
+++ src/pages/AdminPageAddItempage/AdminPageAddItempage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 // import './QueryForm.css'
-import { InputLabel, FormControl, Select, MenuItem, Box, Typography, Button } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -14,6 +14,7 @@ import {
     Form,
     Input,
     Radio,
+    Select 
 } from 'antd';
 import axios from 'axios'; //部署用
 
@@ -69,7 +70,7 @@ export default function AdminPageAddItempage(props) {
         setCourtOpenBeginTime(null)
         setCourtOpenEndTime(null)
     }
-    
+
     const setTimetoString = timedata => {
         return dayjs(timedata).format('HH:mm');
     };
@@ -77,7 +78,7 @@ export default function AdminPageAddItempage(props) {
     const [form] = Form.useForm();
 
     // 使用useNavigate回到上一页
-    const navigate  = useNavigate();
+    const navigate = useNavigate();
 
     // todo:按取消后回到上一页
     const backToLastPage = () => {
@@ -95,19 +96,19 @@ export default function AdminPageAddItempage(props) {
 
     const checkParamsNull = (params) => {
         for (const value of Object.values(params)) {
-          if (value === '' || value === null) {
-            console.log('true')
-            return true;
-          }
+            if (value === '' || value === null) {
+                console.log('true')
+                return true;
+            }
         }
         return false;
-      };
+    };
 
     const insertNewOpenInfo = () => {
 
         const fetchData = async () => {
-            if (courtOpenBeginTime && 
-                courtOpenEndTime && 
+            if (courtOpenBeginTime &&
+                courtOpenEndTime &&
                 courtOpenBeginTime.isAfter(courtOpenEndTime)) {
                 // todo:出现"开始时间不能晚于结束时间"提示框
                 setErrorMsg('开始时间不能晚于结束时间！')
@@ -116,7 +117,7 @@ export default function AdminPageAddItempage(props) {
                 // todo:出现"开始时间不能晚于结束时间"提示框
                 setErrorMsg('请将信息填写完整！')
             }
-            else{
+            else {
                 try {
                     //   console.log(setTimetoString(courtOpenBeginTime))
                     const response = await api.post('/courtOpenInfo/setInfo', params)
@@ -124,16 +125,16 @@ export default function AdminPageAddItempage(props) {
                     if (response.data.code === 200 && response.data.data === true) {
                         // todo:出现“添加成功”提示框
                         console.log('成功了')
-                        setShowSuccessAlert(true)                        
+                        setShowSuccessAlert(true)
                         let timer
                         await new Promise((resolve) => {
-                            timer = setTimeout(() => {                                
+                            timer = setTimeout(() => {
                                 resolve()
                             }, 2000);
-                          });
-                          console.log(timer)
-                          clearTimeout(timer)
-                          backToLastPage()
+                        });
+                        console.log(timer)
+                        clearTimeout(timer)
+                        backToLastPage()
                     }
                 }
                 catch (error) {
@@ -147,11 +148,11 @@ export default function AdminPageAddItempage(props) {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response1 = await api.get('/iteminfo')
-                const response2 = await api.get('/courtinfo')
-                if (response1.data.length !== 0 && response2.data.length !== 0) {
-                    setItemnames(response1.data)
-                    setCourtnames(response2.data)
+                const iteminfoResponse = await api.get('/iteminfo')
+                const courtinfoResponse = await api.get('/courtinfo')
+                if (iteminfoResponse.data.length !== 0 && courtinfoResponse.data.length !== 0) {
+                    setItemnames(iteminfoResponse.data)
+                    setCourtnames(courtinfoResponse.data)
                 }
             }
             catch (error) {
@@ -174,51 +175,78 @@ export default function AdminPageAddItempage(props) {
     }, [showSuccessAlert, errorMsg])
 
     return (
-            <Box>
-                <div style={{ marginTop: '3vh' }}>
-                    <Typography variant="h5" color="primary">填写完成后点击确认即可添加信息</Typography>
-                </div>
-                <div>
-                    <div style={{ height:'6vh' }}>
+        <Box>
+            <div style={{ marginTop: '3vh' }}>
+                <Typography variant="h5" color="primary">填写完成后点击确认即可添加信息</Typography>
+            </div>
+            <div>
+                <div style={{ height: '6vh' }}>
                     {showSuccessAlert && (
-                   <Alert severity="success" sx={{opacity:'0.8'}}>添加信息成功!</Alert>
-                )}
-                {errorMsg && (
-                    <Alert severity="warning" sx={{opacity:'0.8'}}>{errorMsg}</Alert>
-                )}
-                    </div>
-
+                        <Alert severity="success" sx={{ opacity: '0.8' }}>添加信息成功!</Alert>
+                    )}
+                    {errorMsg && (
+                        <Alert severity="warning" sx={{ opacity: '0.8' }}>{errorMsg}</Alert>
+                    )}
                 </div>
 
-                <div>
-                    <Form
-                        form={form}
-                        labelCol={{ span: 4 }}
-                        wrapperCol={{ span: 20 }}
-                        layout="horizontal"
-                        style={{ maxWidth: 1000, textAlign: "left" }}
-                    >
-                        <Form.Item label="種類" name="courtOpenItemId" 
-                        rules={[{ 
+            </div>
+
+            <div>
+                <Form
+                    form={form}
+                    labelCol={{ span: 4 }}
+                    wrapperCol={{ span: 20 }}
+                    layout="horizontal"
+                    style={{ maxWidth: 1000, textAlign: "left" }}
+                >
+                    <Form.Item label="種類" name="courtOpenItemId"
+                        rules={[{
                             required: true,
-                            message:"入力必須項目です" 
-                            }]}>
-                                <Input/>
-                        </Form.Item>
-                        <Form.Item label="ニックネーム" name="nickName" rules={[{ required: true }]}>
-                                <Input/>
-                        </Form.Item>
-                        <Form.Item label="ニックネーム" name="nickName" rules={[{ required: true }]}>
-                                <Input/>
-                        </Form.Item>
-                        <Form.Item label="ニックネーム" name="nickName" rules={[{ required: true }]}>
-                                <Input/>
-                        </Form.Item>
-                        <Form.Item label="ニックネーム" name="nickName" rules={[{ required: true }]}>
-                                <Input/>
-                        </Form.Item>
-                    </Form>
-                </div>
+                            message: "入力必須項目です"
+                        }]}>
+                        <Select>
+                            {/* <Select.Option value="demo">Demo</Select.Option> */}
+                            {(Array.isArray(itemnames)) && itemnames.map((item) =>
+                                (<Select.Option value={String(item.itemInfoId)} key={item.itemInfoId}>{item.itemInfoName}</Select.Option>))}
+                        </Select>
+                    </Form.Item>
+                    <Form.Item label="場所" name="courtOpenCourtId"
+                    rules={[{
+                        required: true,
+                        message: "入力必須項目です"
+                    }]}>
+                        <Select>
+                            {/* <Select.Option value="demo">Demo</Select.Option> */}
+                            {(Array.isArray(courtnames)) && courtnames.map((item) =>
+                                (<Select.Option value={String(item.courtId)} key={item.courtId}>{item.courtName}</Select.Option>))}
+                        </Select>
+                    </Form.Item>
+                    <Form.Item label="週目" name="courtOpenWeekNum"
+                    rules={[{
+                        required: true,
+                        message: "入力必須項目です"
+                    }]}>
+                        <Select>
+                            <Select.Option value="7">毎週</Select.Option>
+                            <Select.Option value="1">一週目</Select.Option>
+                            <Select.Option value="2">二週目</Select.Option>
+                            <Select.Option value="3">三週目</Select.Option>
+                            <Select.Option value="4">四週目</Select.Option>
+                            <Select.Option value="5">五週目</Select.Option>
+                            <Select.Option value="6">六週目</Select.Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item label="ニックネーム" name="nickName" rules={[{ required: true }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="ニックネーム" name="nickName" rules={[{ required: true }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="ニックネーム" name="nickName" rules={[{ required: true }]}>
+                        <Input />
+                    </Form.Item>
+                </Form>
+            </div>
 
 
 
@@ -266,7 +294,7 @@ export default function AdminPageAddItempage(props) {
 
 
 
-                
-            </Box>
+
+        </Box>
     )
 }
